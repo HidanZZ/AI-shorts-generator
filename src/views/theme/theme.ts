c29b27340588d87774d26daeb954c9f1d7f37f91
HidanZZ <aitdaoudelhoussein@gmail.com>
2023-07-27 19:41:01 +0100

    added mui and configured the theme

---
+++ src/views/theme/theme.ts
@@ -0,0 +1,66 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#000000", // Black
+		},
+		secondary: {
+			main: "#ffffff", // White
+		},
+		text: {
+			primary: "#000000", // Black
+			secondary: "#ffffff", // White
+		},
+	},
+	shape: {
+		borderRadius: 0, // Rectangular shape for all components
+	},
+	components: {
+		MuiButton: {
+			styleOverrides: {
+				root: {
+					border: "1px solid #000000", // Black border
+					boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.3)", // Shadow
+					color: "#000000", // Black text
+					backgroundColor: "#ffffff", // White background
+					"&:hover": {
+						backgroundColor: "#000000", // Black background on hover
+						color: "#ffffff", // White text on hover
+					},
+				},
+			},
+		},
+		MuiPaper: {
+			styleOverrides: {
+				root: {
+					border: "1px solid #000000", // Black border
+					boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)", // Slight shadow
+					padding: "10px 20px",
+					margin: "10px",
+					backgroundColor: "#ffffff", // White background
+					color: "#000000", // Black text
+				},
+			},
+		},
+		MuiCard: {
+			styleOverrides: {
+				root: {
+					border: "1px solid #000000", // Black border
+					boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)", // Slight shadow
+				},
+			},
+		},
+		MuiTextField: {
+			styleOverrides: {
+				root: {
+					// border: "1px solid #000000", // Black border
+					boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)", // Slight shadow
+				},
+			},
+		},
+		// Add styleOverrides for any other MUI components you want to customize
+	},
+});
+
+export default theme;
