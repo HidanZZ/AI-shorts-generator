678efc0a61dc57ac9a49b4626f16dd6bd41ae6bc
Ghassanee <ghassane20a@gmail.com>
2023-08-02 23:42:32 +0100

    fix public back

--- back/src/utils/audio.ts
+++ back/src/utils/audio.ts
@@ -1,31 +1,38 @@
-import asyncShell from ".";
-import os from "os";
-import path from "path";
+import asyncShell from '.';
+import os from 'os';
+import path from 'path';
 // run this command edge-tts --text "{text}" --write-media {output_file}
 export const textToSpeech = async (
-	text: string,
-	output_file: string,
-	voice: string = "en-US-ChristopherNeural"
+  text: string,
+  output_file: string,
+  voice: string = 'en-US-ChristopherNeural'
 ): Promise<{ audio: string; subtitles: string }> => {
-	const tempAudioPath = path.join(os.tmpdir(), `${output_file}.mp3`);
-	const tempSubtitlesPath = path.join(os.tmpdir(), `${output_file}.vtt`);
-	const command = `edge-tts --voice en-US-ChristopherNeural  --text "${text}" --write-media ${tempAudioPath} --words-in-cue 1 --write-subtitles ${tempSubtitlesPath} --rate "+5%"`;
-	await asyncShell(command);
-	return {
-		audio: tempAudioPath,
-		subtitles: tempSubtitlesPath,
-	};
+  voice = 'en-CA-LiamNeural';
+  // voice = 'en-GB-RyanNeural'
+  // voice = 'en-US-ChristopherNeural'
+
+  const tempAudioPath = path.join(os.tmpdir(), `${output_file}.mp3`);
+  const tempSubtitlesPath = path.join(
+    os.tmpdir(),
+    `${output_file}.vtt`
+  );
+  const command = `edge-tts --voice ${voice}  --text "${text}" --write-media ${tempAudioPath} --words-in-cue 1 --write-subtitles ${tempSubtitlesPath} --rate "+5%"`;
+  await asyncShell(command);
+  return {
+    audio: tempAudioPath,
+    subtitles: tempSubtitlesPath,
+  };
 };
 
 export async function whisperTranscribe(audioPath: string) {
-	const command = `whisper_timestamped ${audioPath} --model small.en -f vtt --output_dir ${path.dirname(
-		audioPath
-	)}`;
-	console.log("[whisperTranscribe] command", command);
+  const command = `whisper_timestamped ${audioPath} --model small.en -f vtt --output_dir ${path.dirname(
+    audioPath
+  )}`;
+  console.log('[whisperTranscribe] command', command);
 
-	await asyncShell(command);
-	const subtitlesPath = audioPath + ".words.vtt";
-	console.log("[whisperTranscribe] subtitlesPath", subtitlesPath);
+  await asyncShell(command);
+  const subtitlesPath = audioPath + '.words.vtt';
+  console.log('[whisperTranscribe] subtitlesPath', subtitlesPath);
 
-	return subtitlesPath;
+  return subtitlesPath;
 }
